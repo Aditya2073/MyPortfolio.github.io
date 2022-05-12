273fc5a87f808e785b886e0b92776450384af46a
Aditya Rasal <72962577+Aditya2073@users.noreply.github.com>
2022-05-12 21:58:52 +0530

    Add files via upload

---
+++ My Portfolio Website/js/scripts.js
@@ -0,0 +1,82 @@
+/*!
+* Start Bootstrap - Resume v7.0.3 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2021 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+
+window.addEventListener("DOMContentLoaded", function () {
+    // get the form elements defined in your form HTML above
+  
+    var form = document.getElementById("my-form");
+    // var button = document.getElementById("my-form-button");
+    var status = document.getElementById("status");
+  
+    // Success and Error functions for after the form is submitted
+  
+    function success() {
+      form.reset();
+      status.classList.add("success");
+      status.innerHTML = "Thanks! I will reply to you soon";
+    }
+  
+    function error() {
+      status.classList.add("error");
+      status.innerHTML = "Oops! There was a problem.";
+    }
+  
+    // handle the form submission event
+  
+    form.addEventListener("submit", function (ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+  
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
+  
